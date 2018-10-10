98dbb8003da124d647a1096f5ec7468c1cbc36e8
lfhwnqe <464385492@qq.com>
2018-10-10 11:11:36 +0800

    增加console

--- app/controller/user.js
+++ app/controller/user.js
@@ -5,6 +5,7 @@ const Controller = require('egg').Controller;
 class UserController extends Controller {
     async getUserInfo() {
         const data = this.ctx.request.body
+        console.log('data is back==>>', data)
         this.ctx.body = {
             data
         }
